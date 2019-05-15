08f7f3668a3d3abf03a303b199afcc2a6096322d
cristobal artigas <artigas.cristobal@gmail.com>
2019-05-15 19:37:18 +0200

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restos = Restaurant.create([
+  {
+    name: 'Goiko',
+    address: "Av sda",
+    phone_number: "123456",
+    category: "chinese"
+  },
+  {
+    name: 'Usa',
+    address: "Av federico",
+    phone_number: "22222",
+    category: "italian"
+  },
+  {
+    name: 'Chile',
+    address: "Av colon",
+    phone_number: "33333",
+    category: "japanese"
+  },
+  {
+    name: 'Panama',
+    address: "Av rambla",
+    phone_number: "44444",
+    category: "french",
+  },
+  {
+    name: 'Argentina',
+    address: "Av macke",
+    phone_number: "5555",
+    category: "belgian",
+  }])
